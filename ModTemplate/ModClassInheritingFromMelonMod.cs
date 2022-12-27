08fc9aa809d751aa47bae1b9f962c453ca4dbb3a
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-27 15:32:00 -0500

    Update template

---
+++ ModTemplate/ModClassInheritingFromMelonMod.cs
@@ -0,0 +1,11 @@
+﻿using MelonLoader;
+
+namespace ModNamespace;
+internal sealed class ModClassInheritingFromMelonMod : MelonMod
+{
+	public override void OnInitializeMelon()
+	{
+		//Any initialization code goes here.
+		//This method can be deleted if no initialization is required.
+	}
+}
--- ModTemplate/Properties/AssemblyInfo.cs
+++ ModTemplate/Properties/AssemblyInfo.cs
@@ -1,20 +1,25 @@
 using MelonLoader;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 //This is a C# comment. Comments have no impact on compilation.
 
-//ModName, ModVersion, ModAuthor, and ModNamespace.ModClassInheritingFromMelonMod all need changed.
+[assembly: AssemblyTitle(BuildInfo.ModName)]
+[assembly: AssemblyCopyright($"Created by ModAuthor")]
 
-[assembly: AssemblyTitle("ModName")]
-[assembly: AssemblyCopyright("Created by ModAuthor")]
-
-//Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
-//Mods typically use 3 numbers. For example: 1.2.1
-//The mod version need specified in three places.
-[assembly: AssemblyVersion("ModVersion")]
-[assembly: AssemblyFileVersion("ModVersion")]
-[assembly: MelonInfo(typeof(ModNamespace.ModClassInheritingFromMelonMod), "ModName", "ModVersion", "ModAuthor")]
+[assembly: AssemblyVersion(BuildInfo.ModVersion)]
+[assembly: AssemblyFileVersion(BuildInfo.ModVersion)]
+[assembly: MelonInfo(typeof(ModNamespace.ModClassInheritingFromMelonMod), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.ModAuthor)]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
+
+internal static class BuildInfo
+{
+	internal const string ModName = "Name";
+	internal const string ModAuthor = "Author";
+	/// <summary>
+	/// Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
+	/// Mods typically use 3 numbers. For example: 1.2.1
+	/// </summary>
+	internal const string ModVersion = "1.0.0";
+}
